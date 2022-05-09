c5dc53279be946a86edd7feefff233f13b99ee8a
지노지노_정진호 <zino@afreecatv.com>
2022-05-10 01:03:34 +0900

    feat: 로또티켓의 번호를 생성하기위한 클래스 생성 및 테스트할 코드 작성

---
+++ Lotto/Lotto/LottoMachine/LottoNumberGenerator.swift
@@ -0,0 +1,15 @@
+//
+//  LottoNumberGenerator.swift
+//  Lotto
+//
+//  Created by jinho jeong on 2022/05/10.
+//
+
+import Foundation
+
+final class LottoNumberGenerator {
+    
+    func generateLottoTicket(of ticketCount: Int) -> [[Int]] {
+      return [[]]
+    }  
+}
---
+++ Lotto/LottoTests/LottoNumberGeneratorTests.swift
@@ -0,0 +1,23 @@
+//
+//  LottoNumberGeneratorTests.swift
+//  LottoTests
+//
+//  Created by jinho jeong on 2022/05/10.
+//
+
+import XCTest
+
+class LottoNumberGeneratorTests: XCTestCase {
+
+    let sut: LottoNumberGenerator = LottoNumberGenerator()
+    
+    func test_given로또발급갯수_when갯수에따른발급_then발급된로또배열의카운트가_같은지체크() {
+       
+       
+    }
+    
+    func test_given_하나의로또번호_when중복번호확인_then중복번호가있다면넘어가야함() {
+        
+    }
+    
+}
