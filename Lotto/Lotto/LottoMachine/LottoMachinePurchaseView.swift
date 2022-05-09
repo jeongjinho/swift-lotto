48f0825f387075af308d61b7d89ee67b6b7387e5
지노지노_정진호 <zino@afreecatv.com>
2022-05-10 01:50:38 +0900

    feat:  로또 구매화면에 대한 에러 타입정의

--- Lotto/Lotto/LottoMachine/LottoMachinePurchaseView.swift
+++ Lotto/Lotto/LottoMachine/LottoMachinePurchaseView.swift
@@ -7,21 +7,49 @@
 
 import Foundation
 
-enum PurchaseError: Error {
+enum PurchaseError: LocalizedError {
     case notNumber
     case remainer
 }
 
+extension PurchaseError {
+    var errorDescription: String? {
+        switch self {
+        case .notNumber:
+            return "숫자로 변환되지 않는 입력값입니다."
+        case .remainer:
+            return "천원단위로 구매가능합니다."
+        }
+    }
+}
+
+
+
 struct LottoMachinePurchaseView {
-        
-    func purchaseLotto() throws -> Int {
-        guard let input = readLine(), let amount = Int(input) else { throw PurchaseError.notNumber }
-        guard amount % 1000 == 0 else { throw PurchaseError.remainer }
-        printPurchasedLotto(of: amount / 1000)
-        return amount / 1000
+    private let lottoGenerator: LottoNumberGenerator = LottoNumberGenerator()
+    
+    func purchaseLotto() -> Lotto {
+        do {
+            guard let input = readLine(), let amount = Int(input) else { throw PurchaseError.notNumber }
+            guard amount % 1000 == 0 else { throw PurchaseError.remainer }
+            let gameCount = amount / 1000
+            printPurchasedLotto(of: gameCount)
+            let lotto = lottoGenerator.generateLotto(of: gameCount)
+            printLotto(lotto)
+            return lotto
+        } catch let error {
+            print(error.localizedDescription)
+        }
     }
     
     private func printPurchasedLotto(of count: Int) {
         print("\(count)개를 구매하였습니다.")
+    }
+    
+    private func printLotto(_ lotto: Lotto) {
+        for game in lotto.games {
+            print(game)
+            print("\n")
+        }
     }
 }
